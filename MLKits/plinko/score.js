a4cf3f46c49a928c088ce93ca926e3a14b35e61f
alfredlucero <alfred.lucero@ucla.edu>
2020-12-23 23:43:31 -0800

    Finished plinko example with multiple features and normalization

--- MLKits/plinko/score.js
+++ MLKits/plinko/score.js
@@ -12,22 +12,31 @@ function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
 
 function runAnalysis() {
   const testSetSize = 100;
-  const [testSet, trainingSet] = splitDataset(outputs, testSetSize);
+  const k = 10;
 
-  _.range(1, 20).forEach((k) => {
+  _.range(0, 3).forEach((feature) => {
+    const data = _.map(outputs, (row) => [row[feature], _.last(row)]);
+    const [testSet, trainingSet] = splitDataset(minMax(data, 1), testSetSize);
+
     const accuracy = _.chain(testSet)
-      .filter((testPoint) => knn(trainingSet, testPoint[0], k) === testPoint[3])
+      .filter(
+        (testPoint) =>
+          knn(trainingSet, _.initial(testPoint), k) === _.last(testPoint)
+      )
       .size()
       .divide(testSetSize)
       .value();
 
-    console.log("For k of", k, "Accuracy:", accuracy);
+    console.log("For feature of", feature, "Accuracy:", accuracy);
   });
 }
 
+// Point has 3 values without the label
 function knn(data, point, k) {
   return _.chain(data)
-    .map((row) => [distance(row[0], point), row[3]])
+    .map((row) => {
+      return [distance(_.initial(row), point), _.last(row)];
+    })
     .sortBy((row) => row[0])
     .slice(0, k)
     .countBy((row) => row[1])
@@ -39,8 +48,17 @@ function knn(data, point, k) {
     .value();
 }
 
+// Using pythagorean theorem
 function distance(pointA, pointB) {
-  return Math.abs(pointA - pointB);
+  // Rather than pointA = 300, pointB = 250
+  // pointA = [300, .5, 16], pointB = [350, .55, 16]
+  return (
+    _.chain(pointA)
+      .zip(pointB)
+      .map(([a, b]) => (a - b) ** 2)
+      .sum()
+      .value() ** 0.5
+  );
 }
 
 function splitDataset(data, testCount) {
@@ -50,4 +68,22 @@ function splitDataset(data, testCount) {
   const trainingSet = _.slice(shuffled, testCount);
 
   return [testSet, trainingSet];
+}
+
+// Normalizes the data from scale of 0 to 1
+function minMax(data, featureCount) {
+  const clonedData = _.cloneDeep(data);
+
+  for (let i = 0; i < featureCount; i++) {
+    const column = clonedData.map((row) => row[i]);
+
+    const min = _.min(column);
+    const max = _.max(column);
+
+    for (let j = 0; j < clonedData.length; j++) {
+      clonedData[j][i] = (clonedData[j][i] - min) / (max - min);
+    }
+  }
+
+  return clonedData;
 }
